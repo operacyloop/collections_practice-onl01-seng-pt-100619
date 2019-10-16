d8ae29e184634878ee8ecc98fb5e9ae37ea88219
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-16 04:15:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
